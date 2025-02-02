7dcc9b953d13b40d1d358c82cfb99eb48841e40f
siva <kvsprasad0987@gmail.com>
2025-02-02 15:14:30 +0530

    changes in js

--- home.js
+++ home.js
@@ -3,11 +3,8 @@ function alt(){
 };
 setTimeout(alt,2000);
 setTimeout(() => {
-    // Display a confirmation dialog after 5 seconds
     const userResponse = confirm('Would you like to allow notifications?');
-    
     if (userResponse) {
-      // If user clicks "OK"
       const email = prompt("Enter your email:");
       if (email) {
         alert(`Thank you! You entered: ${email}`);
@@ -15,7 +12,6 @@ setTimeout(() => {
         alert("You didn't provide an email.");
       }
     } else {
-      // If user clicks "Cancel"
       alert("NOT A PROBLEM");
     }
   }, 5000);
